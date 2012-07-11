36136a4af4030f63291e9f6a0f4dac1a5ed0a4c9
andercrema80@gmail.com <andercrema80@gmail.com@0d7c553c-a400-1cfc-08e2-961a471d1b27>
2012-07-11 20:34:14 +0000

    Se completo la programación, por corregir el tema de la conexión a la Base de Datos por lo cual se genera el error.

---
+++ trunk/Solucion/SOAPService/Residentes.svc.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using SOAPService.Persistencia;
+using SOAPService.Dominio;
+
+namespace SOAPService
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Residentes" en el código, en svc y en el archivo de configuración a la vez.
+    public class Residentes : IResidentes
+    {
+        private ResidenteDAO residenteDAO = null;
+
+        private ResidenteDAO ResidenteDAO
+        {
+            get
+            {
+                if (residenteDAO == null)
+                    residenteDAO = new ResidenteDAO();
+                return residenteDAO;
+            }
+        }
+
+        public DResidente CrearResidente(DResidente dresidente)
+        {
+            DResidente residenteACrear = new DResidente()
+            {
+                DNI = dresidente.DNI,
+                Nombres = dresidente.Nombres,
+                ApellidoPaterno = dresidente.ApellidoMaterno,
+                ApellidoMaterno = dresidente.ApellidoMaterno,
+                Edad = dresidente.Edad,
+                Correo = dresidente.Correo,
+                Clave = dresidente.Clave,
+                Tipo = dresidente.Tipo
+            };
+
+            return residenteDAO.Crear(residenteACrear);
+        }
+    }
+}
