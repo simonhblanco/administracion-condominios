de73e04073088b4d3e4630f126678ecf22f3e352
andercrema80@gmail.com <andercrema80@gmail.com@0d7c553c-a400-1cfc-08e2-961a471d1b27>
2012-07-13 16:30:06 +0000

    Paso la Fabrica de Sesiones pero ahora tengo problemas con el Proveedor SQL

--- trunk/Solucion/SOAPService/Persistencia/ConexionUtil.cs
+++ trunk/Solucion/SOAPService/Persistencia/ConexionUtil.cs
@@ -10,7 +10,8 @@ namespace SOAPService.Persistencia
         public static string ObtenerCadena()
         {
             //return "Data Source=447587A2423/SQLEXPRESS;AttachDbFilename='D:/User/DValenzuela/UPC/04Ciclo/ DESARROLLO PARA ENTORNO WEB/CONDOMINIO.mdf';Integrated Security=True";
-            return "Data Source=ANDERCREMA;Initial Catalog=CONDOMINIO;User ID=sa;Password=***********";
+            //return "Data Source=ANDERCREMA;Initial Catalog=CONDOMINIO;User ID=sa;Password=***********";
+            return "Data Source=localhost;Initial Catalog=Condominio;User Id=condominio;Password=condominio; Integrated Security=True";
         }
     }
 }
