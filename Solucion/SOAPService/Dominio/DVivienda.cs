d9b829741ce5081c95e19c559c0eb9ee7f47ffd7
andercrema80@gmail.com <andercrema80@gmail.com@0d7c553c-a400-1cfc-08e2-961a471d1b27>
2012-07-27 17:48:53 +0000

    Realizando Vivienda en SOA

--- Solucion/SOAPService/Dominio/DVivienda.cs
+++ Solucion/SOAPService/Dominio/DVivienda.cs
@@ -9,54 +9,53 @@ namespace SOAPService.Dominio
     [DataContract]  //contrato
     public class DVivienda
     {
-            private int _NumVivienda;
-            private String _Ubicacion;
-            private int _Numero;
-            private int _Metraje;
-            private String _Tipo;
-            private DResidente _Residente;
+        private int _NumVivienda;
+        private String _Ubicacion;
+        private int _Numero;
+        private int _Metraje;
+        private String _Tipo;
+        private DResidente _Residente;
 
-            [DataMember]
-            public int NumVivienda
-            {
-                get { return _NumVivienda; }
-                set { _NumVivienda = value; }
-            }
+        [DataMember]
+        public int NumVivienda
+        {
+            get { return _NumVivienda; }
+            set { _NumVivienda = value; }
+        }
 
-            [DataMember]
-            public String Ubicacion
-            {
-                get { return _Ubicacion; }
-                set { _Ubicacion = value; }
-            }
+        [DataMember]
+        public String Ubicacion
+        {
+            get { return _Ubicacion; }
+            set { _Ubicacion = value; }
+        }
 
-            [DataMember]
-            public int Numero
-            {
-                get { return _Numero; }
-                set { _Numero = value; }
-            }
+        [DataMember]
+        public int Numero
+        {
+            get { return _Numero; }
+            set { _Numero = value; }
+        }
 
-            [DataMember]
-            public int Metraje
-            {
-                get { return _Metraje; }
-                set { _Metraje = value; }
-            }
+        [DataMember]
+        public int Metraje
+        {
+            get { return _Metraje; }
+            set { _Metraje = value; }
+        }
 
-            [DataMember]
-            public String Tipo
-            {
-                get { return _Tipo; }
-                set { _Tipo = value; }
-            }
+        [DataMember]
+        public String Tipo
+        {
+            get { return _Tipo; }
+            set { _Tipo = value; }
+        }
 
-            [DataMember]
-            public DResidente Residente
-            {
-                get { return _Residente; }
-                set { _Residente = value; }
-            }
-       
+        [DataMember]
+        public DResidente Residente
+        {
+            get { return _Residente; }
+            set { _Residente = value; }
+        }
     }
 }
