e326d2163f792e8ee3b6279e8a6af683d2cdfa2e
viannesse <viannesse@0d7c553c-a400-1cfc-08e2-961a471d1b27>
2012-08-09 04:02:02 +0000

    Se realizo las pruebas de Residentes con su respectivo Servicio

---
+++ trunk/Solucion/SOAPServiceTest/TestServResidente.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SOAPServiceTest
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class TestServResidente
+    {
+        SRResidente.ResidentesClient residenteWS = new SRResidente.ResidentesClient();
+
+        SRResidente.DResidente residente = null;
+
+        [TestMethod]
+        public void Test01Crear()
+        {
+            residente = new SRResidente.DResidente();
+
+            residente.DNI = "07635076";
+            residente.ApellidoPaterno = "Valenzuela";
+            residente.ApellidoMaterno = "Ñiquen";
+            residente.Nombres = "Ander";
+            residente.Edad = 30;
+            residente.Correo = "abc@campus.edu";
+            residente.Clave = "1234";
+            residente.Tipo = "R";
+
+            residente = residenteWS.CrearResidente(residente);
+
+            Assert.IsNotNull(residente);
+            Assert.IsTrue(residente.Edad > 0);
+        }
+
+        [TestMethod]
+        public void Test02Obtener()
+        {
+            residente = new SRResidente.DResidente();
+
+            residente.DNI = "07635076";
+
+            residente = residenteWS.ObtenerResidente(residente.DNI);
+
+            Assert.IsNotNull(residente);
+            Assert.AreEqual(residente.DNI, "07635076");
+            Assert.IsNotNull(residente.Correo);
+        }
+
+        [TestMethod]
+        public void Test03Modificar()
+        {
+            residente = new SRResidente.DResidente();
+
+            residente.DNI = "07635076";
+            residente = residenteWS.ObtenerResidente(residente.DNI);
+
+            SRResidente.DResidente residenteOriginal = residenteWS.ObtenerResidente(residente.DNI);
+
+            residente.Nombres = "Anderson";
+
+            SRResidente.DResidente resindeteModificado = residenteWS.ModificarResidente(residente);
+
+            Assert.IsNotNull(resindeteModificado);
+            Assert.AreNotEqual(residenteOriginal.Nombres, resindeteModificado.Nombres);
+        }
+
+        [TestMethod]
+        public void Test04Listar()
+        {
+            ICollection<SRResidente.DResidente> LDResidente = residenteWS.ListarTodosLosResidentes();
+            Assert.IsNotNull(LDResidente);
+            Assert.IsTrue(LDResidente.Count > 0);
+        }
+
+        [TestMethod]
+        public void Test05Eliminar()
+        {
+            residente = new SRResidente.DResidente();
+
+            residente.DNI = "07635076";
+
+            //Assert.DoesNotThrow(delegate
+            //{
+            residenteWS.EliminarResidente(residente.DNI);
+            //});
+
+            residente = residenteWS.ObtenerResidente(residente.DNI);
+            Assert.IsNull(residente);
+        }
+    }
+}
